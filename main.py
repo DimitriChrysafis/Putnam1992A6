58674fdf1e2e2a832a3b7267ad9897fa5ccac653
DimitriChrysafis <Dimitri.chrysafis@gmail.com>
2024-06-07 09:29:09 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from visualize_tetrahedron import plotToMakeTetra
+from simulation import compileSize
+
+def main():
+    plotToMakeTetra()
+    compileSize()
+
+if __name__ == "__main__":
+    main()
+
